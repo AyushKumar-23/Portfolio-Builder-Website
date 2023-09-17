a84c01ff5d6034c3ca08fbd022e49fbc6f4526d8
Ayush Kumar <kumarayush009@gmail.com>
2023-09-17 19:06:11 +0530

    Changed static settings

--- PortfolioBuilder/urls.py
+++ PortfolioBuilder/urls.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^download/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$',serve,{'document_root':settings.STATIC_ROOT}),
+
     path('admin/', admin.site.urls),
     path('',include('portfolio.urls')),
     path("user/",include("userapp.urls")),
     path("user/",include("django.contrib.auth.urls")),
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root': settings.MEDIA_ROOT}),
 ]
